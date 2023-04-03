6e0e4743fd85f6766a1da50d22910e8808b58a0d
Aristides Pascoal Queta <aristidesquetajr@gmail.com>
2023-04-03 17:26:27 +0100

    router configuration

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,9 @@
-const n: number = 8
+import express from 'express'
+import { routes } from './routes'
 
+const app = express()
+
+app.use(express.json())
+app.use(routes)
+
+app.listen(3000, () => console.log('Running in port 3000'))
